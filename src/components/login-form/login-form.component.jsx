91c8ad3057926c583e12ffd25e2c2622d995e1ac
Jackson Ikpe <Jikpe@wragbysolutions.com>
2023-01-19 23:23:01 +0100

    updated login page

--- src/components/login-form/login-form.component.jsx
+++ src/components/login-form/login-form.component.jsx
@@ -1,16 +1,29 @@
+import { ReactComponent as Eye } from "../../assets/img/eye.svg";
 import "./login-form.style.scss";
 
 const LoginForm = () => {
   return (
     <form className="login-form">
-      <p>Log in to your account</p>
-      <p>Don't have an account? Get Started</p>
+      <p className="info">Log in to your account</p>
+      <p className="noAccountInfo">
+        Don't have an account? <span>Get Started</span>
+      </p>
       <div className="container">
         <label htmlFor="email">Email</label>
-        <input type="email" placeholder="E.g: victor.onazi@getbani.co" />
-        <label>Password</label>
-        <input type="password" placeholder="Password" />
-        <button>Log in</button>
+        <input
+          type="email"
+          placeholder="E.g: victor.onazi@getbani.co"
+          required
+        />
+        <div className="password">
+          <label>Password</label>
+          <a href="#">Forgot password?</a>
+        </div>
+        <div className="passwordInput">
+          <input type="password" placeholder="Password" required />
+          <Eye className="eye" />
+        </div>
+        <button className="loginButton">Log in</button>
       </div>
     </form>
   );
